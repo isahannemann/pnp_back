49ac5431154da7557783f182a789899e65806aac
isahannemann <isadorahannemann@gmail.com>
2025-01-29 20:53:32 -0300

    feat: tutor controller

---
+++ src/main/java/com/peaktech/pnp/api/controller/TutorController.java
@@ -0,0 +1,31 @@
+﻿package com.peaktech.pnp.api.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("tutor")
+public class TutorController {
+
+    @PostMapping
+    public ResponseEntity<?> save(@Valid @RequestBody tutorInput tutorInput) {
+        if (tutorService.findByEmail(tutorInput.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("Email já cadastrado");
+
+        } else if (tutorInput.getCpf() != null && tutorService.findByCpf(tutorInput.getCpf()).isPresent()) {
+            return ResponseEntity.badRequest().body("Cpf já cadastrado");
+
+        } else {
+            Tutor createdTutor = TutorService.save(tutorInput);
+            TutorOutput userOutput = new TutorOutput(createdTutor);
+            return ResponseEntity.ok(userOutput);
+        }
+    }
+
+}
