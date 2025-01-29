76640441831eaa2c84a25fa220c97b68da2f8fb1
isahannemann <isadorahannemann@gmail.com>
2025-01-29 18:29:34 -0300

    feat: pnpapplication

---
+++ src/main/java/com/peaktech/pnp/PnpApplication.java
@@ -0,0 +1,13 @@
+package com.peaktech.pnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnpApplication.class, args);
+	}
+
+}
