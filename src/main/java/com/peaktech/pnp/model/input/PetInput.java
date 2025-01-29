5a428ec89d87be0f47d024298744fd721040f7f4
isahannemann <isadorahannemann@gmail.com>
2025-01-29 18:25:52 -0300

    feat: petInput

---
+++ src/main/java/com/peaktech/pnp/model/input/PetInput.java
@@ -0,0 +1,32 @@
+package com.peaktech.pnp.model.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.lang.Nullable;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class PetInput {
+
+    private String name;
+    private Date nascimento;
+    private String tutor;
+    private String banho;
+    private String alimentacao;
+    private String vacina;
+    private String vermifugo;
+    private String medicamento;
+    private String obs;
+
+
+    @Nullable
+    private String foto;
+
+}
