22ecb622987d9b264d642171bc4ca1ef57fea4bd
isahannemann <isadorahannemann@gmail.com>
2025-01-29 18:24:59 -0300

    feat: defaultEntityDTO.java

---
+++ src/main/java/com/peaktech/pnp/model/defaults/DefaultEntityDTO.java
@@ -0,0 +1,8 @@
+package com.peaktech.pnp.model.defaults;
+
+import java.io.Serializable;
+
+public abstract class DefaultEntityDTO implements Serializable {
+    private Long id;
+    private Boolean actived;
+}
