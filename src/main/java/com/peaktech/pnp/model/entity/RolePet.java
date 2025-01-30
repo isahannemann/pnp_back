011d3b743adab2c4a6cea80c42b1304220074681
isahannemann <isadorahannemann@gmail.com>
2025-01-30 09:09:34 -0300

    feat: rolepet entity

---
+++ src/main/java/com/peaktech/pnp/model/entity/RolePet.java
@@ -0,0 +1,24 @@
+﻿package com.peaktech.pnp.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rolespet")
+public class RolePet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_seq_rolespet")
+    @SequenceGenerator(sequenceName = "id_seq_rolespet", allocationSize = 1, name = "id_seq_rolespet")
+    private Long id;
+
+    @Size(max = 60)
+    @NotNull
+    @Column(name = "rolepet", length = 60)
+    private String role;
+}
