23ced3d7d806d641c1b1aa2a92da197132cdbeb8
isahannemann <isadorahannemann@gmail.com>
2025-01-30 13:57:26 -0300

    feat: uploadStrategy

---
+++ src/main/java/com/peaktech/pnp/api/service/UploadStrategy.java
@@ -0,0 +1,10 @@
+﻿package com.peaktech.pnp.api.service;
+
+import com.peaktech.pnp.model.input.FileUploadInput;
+import com.peaktech.pnp.model.output.PhotoOutput;
+
+public interface UploadStrategy {
+    String saveFile(FileUploadInput request);
+    PhotoOutput getFile(String filename) throws IOException;
+    boolean deleteFile(String filename);
+}
