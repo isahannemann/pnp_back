c7368598d7b56bb7db79733bbbadf46cb33d0b33
isahannemann <isadorahannemann@gmail.com>
2025-01-30 08:05:22 -0300

    feat: roteTutorRepository

---
+++ src/main/java/com/peaktech/pnp/repository/RoleTutorRepository.java
@@ -0,0 +1,11 @@
+﻿package com.peaktech.pnp.repository;
+
+import com.peaktech.pnp.model.entity.RoleTutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleTutorRepository extends JpaRepository<RoleTutor, Long> {
+
+}
+
