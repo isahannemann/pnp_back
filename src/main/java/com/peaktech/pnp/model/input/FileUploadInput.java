8380899717303a4a11728ec39af5e70903f49e85
isahannemann <isadorahannemann@gmail.com>
2025-01-30 09:11:47 -0300

    feat: carrega arquivo input

---
+++ src/main/java/com/peaktech/pnp/model/input/FileUploadInput.java
@@ -0,0 +1,16 @@
+﻿package com.peaktech.pnp.model.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class FileUploadInput {
+    private String fileName;
+    private String pdfBase64;
+}
