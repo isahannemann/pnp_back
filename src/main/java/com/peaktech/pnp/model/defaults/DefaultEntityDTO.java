3c64e1ca9d14f1578a22510f8df9cc89324fc890
isahannemann <isadorahannemann@gmail.com>
2025-01-30 09:07:10 -0300

    feat: idTutor

--- src/main/java/com/peaktech/pnp/model/defaults/DefaultEntityDTO.java
+++ src/main/java/com/peaktech/pnp/model/defaults/DefaultEntityDTO.java
@@ -1,8 +1,12 @@
 package com.peaktech.pnp.model.defaults;
 
+import lombok.Data;
 import java.io.Serializable;
 
+@Data
 public abstract class DefaultEntityDTO implements Serializable {
-    private Long id;
-    private Boolean actived;
+    private Long idPet;
+    private Long idTutor;
+    private Boolean activedPet;
+    }
 }
