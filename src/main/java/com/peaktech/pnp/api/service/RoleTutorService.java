b4d6b501707b1915f24aa5207761bc5a47d73e58
isahannemann <isadorahannemann@gmail.com>
2025-01-30 08:06:02 -0300

    feat: roleTutorService

---
+++ src/main/java/com/peaktech/pnp/api/service/RoleTutorService.java
@@ -0,0 +1,44 @@
+﻿package com.peaktech.pnp.api.service;
+
+import com.peaktech.pnp.model.entity.RoleTutor;
+import com.peaktech.pnp.repository.RoleTutorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoleTutorService {
+    @Autowired
+    private final RoleTutorRepository roleTutorRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public RoleTutor findById(Long id) {
+        return roleTutorRepository.findById(id).orElseThrow(() -> new RuntimeException("Role não encontrada"));
+    }
+
+    public List<RoleTutor> listAllRole() {
+        return roleTutorRepository.findAll();
+    }
+
+    public RoleTutor save(RoleTutorInput roleTutorInput) {
+        RoleTutor roleTutor = modelMapper.map(roleTutorInput, RoleTutor.class);
+        roleTutor.setRoleTutor(roleTutorInput.getRoleTutor().toUpperCase());
+        return roleTutorRepository.save(roleTutor);
+    }
+
+    public RoleTutor updateById(Long id, RoleTutorInput roleTutorInput) {
+        RoleTutor roleTutor = findById(id);
+        modelMapper.map(roleTutorInput, roleTutor);
+        roleTutor.setRoleTutor(roleTutorInput.getRoleTutor().toUpperCase());
+        return roleTutorRepository.save(roleTutor);
+    }
+
+    public void desactivateById(Long id) {
+        RoleTutor roleTutor = findById(id);
+        roleTutorRepository.delete(roleTutor);
+    }
+}
