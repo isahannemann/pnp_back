5960e5b86eb455959ef71a5c706d0b1b0981652e
isahannemann <isadorahannemann@gmail.com>
2025-01-30 13:39:37 -0300

    feat: tutorPhotoOutputStrategy

---
+++ src/main/java/com/peaktech/pnp/model/output/TutorPhotoOutputStrategy.java
@@ -0,0 +1,18 @@
+﻿package com.peaktech.pnp.model.output;
+
+import com.peaktech.pnp.model.output.PhotoOutput;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TutorPhotoOutputStrategy implements PhotoOutputStrategy {
+
+    private final FileService fileService = new FileService();
+    private static final String TUTOR_DIRECTORY = "/caminho/para/tutor";
+
+    @Override
+    public PhotoOutput getPhoto(String filename) throws IOException {
+        String base64 = fileService.getBase64File(filename, TUTOR_DIRECTORY);
+        String format = fileService.getMimeType(fileService.downloadFile(filename, TUTOR_DIRECTORY));
+        return new PhotoOutput(base64, format);
+    }
+}
