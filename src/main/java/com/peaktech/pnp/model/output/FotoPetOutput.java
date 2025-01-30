dd442db8ee2b09d5385f93b06bc53cd1d5dc4872
isahannemann <isadorahannemann@gmail.com>
2025-01-30 09:10:40 -0300

    feat: fotopetOutput
    Adiciona: fotoBase64Pet e formatoFotoPet

---
+++ src/main/java/com/peaktech/pnp/model/output/FotoPetOutput.java
@@ -0,0 +1,14 @@
+﻿package com.peaktech.pnp.model.output;
+
+import com.peaktech.pnp.model.defaults.DefaultEntityDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FotoPetOutput extends DefaultEntityDTO {
+    private String fotoBase64Pet;
+    private String formatoFotoPet;
+}
